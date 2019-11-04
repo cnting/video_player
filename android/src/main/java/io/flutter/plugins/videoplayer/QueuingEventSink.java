ac1a78f96325b98116882afe2d606b3db8324b7b
cnting <cnting@CTMacBook-Pro.local>
2019-11-04 15:18:02 +0800

    flutter版本升到v1.9，event需要在主线程中发送

--- android/src/main/java/io/flutter/plugins/videoplayer/QueuingEventSink.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/QueuingEventSink.java
@@ -1,6 +1,11 @@
 package io.flutter.plugins.videoplayer;
 
+
+import android.os.Handler;
+import android.os.Looper;
+
 import io.flutter.plugin.common.EventChannel;
+
 import java.util.ArrayList;
 
 /**
@@ -13,69 +18,81 @@ import java.util.ArrayList;
  * externally.
  */
 final class QueuingEventSink implements EventChannel.EventSink {
-  private EventChannel.EventSink delegate;
-  private ArrayList<Object> eventQueue = new ArrayList<>();
-  private boolean done = false;
+    private EventChannel.EventSink delegate;
+    private ArrayList<Object> eventQueue = new ArrayList<>();
+    private boolean done = false;
+    private Handler handler;
 
-  public void setDelegate(EventChannel.EventSink delegate) {
-    this.delegate = delegate;
-    maybeFlush();
-  }
-
-  @Override
-  public void endOfStream() {
-    enqueue(new EndOfStreamEvent());
-    maybeFlush();
-    done = true;
-  }
-
-  @Override
-  public void error(String code, String message, Object details) {
-    enqueue(new ErrorEvent(code, message, details));
-    maybeFlush();
-  }
-
-  @Override
-  public void success(Object event) {
-    enqueue(event);
-    maybeFlush();
-  }
-
-  private void enqueue(Object event) {
-    if (done) {
-      return;
+    QueuingEventSink() {
+        handler = new Handler(Looper.getMainLooper());
     }
-    eventQueue.add(event);
-  }
 
-  private void maybeFlush() {
-    if (delegate == null) {
-      return;
+    void setDelegate(EventChannel.EventSink delegate) {
+        this.delegate = delegate;
+        maybeFlush();
     }
-    for (Object event : eventQueue) {
-      if (event instanceof EndOfStreamEvent) {
-        delegate.endOfStream();
-      } else if (event instanceof ErrorEvent) {
-        ErrorEvent errorEvent = (ErrorEvent) event;
-        delegate.error(errorEvent.code, errorEvent.message, errorEvent.details);
-      } else {
-        delegate.success(event);
-      }
+
+    @Override
+    public void endOfStream() {
+        enqueue(new EndOfStreamEvent());
+        maybeFlush();
+        done = true;
     }
-    eventQueue.clear();
-  }
 
-  private static class EndOfStreamEvent {}
-
-  private static class ErrorEvent {
-    String code;
-    String message;
-    Object details;
-
-    ErrorEvent(String code, String message, Object details) {
-      this.code = code;
-      this.message = message;
-      this.details = details;
+    @Override
+    public void error(String code, String message, Object details) {
+        enqueue(new ErrorEvent(code, message, details));
+        maybeFlush();
     }
-  }
+
+    @Override
+    public void success(Object event) {
+        enqueue(event);
+        maybeFlush();
+    }
+
+    private void enqueue(Object event) {
+        if (done) {
+            return;
+        }
+        eventQueue.add(event);
+    }
+
+    private void maybeFlush() {
+        if (delegate == null) {
+            return;
+        }
+        for (Object event : eventQueue) {
+            if (event instanceof EndOfStreamEvent) {
+                delegate.endOfStream();
+            } else if (event instanceof ErrorEvent) {
+                ErrorEvent errorEvent = (ErrorEvent) event;
+                handler.post(() -> {
+                    if (delegate != null)
+                        delegate.error(errorEvent.code, errorEvent.message, errorEvent.details);
+                });
+            } else {
+                handler.post(() -> {
+                    if (delegate != null)
+                        delegate.success(event);
+                });
+            }
+        }
+        eventQueue.clear();
+    }
+
+    private static class EndOfStreamEvent {
+    }
+
+    private static class ErrorEvent {
+        String code;
+        String message;
+        Object details;
+
+        ErrorEvent(String code, String message, Object details) {
+            this.code = code;
+            this.message = message;
+            this.details = details;
+        }
+    }
 }
